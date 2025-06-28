1c6402727e4a452f0551c4c0f0800695ef3441e2
Sonub150 <nigamsonu150@gmail.com>
2025-06-28 17:59:19 +0530

    start

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
+
+export default function App() {
+  
+  return (
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home/>}></Route>
+    <Route path='/sign-in' element={<Signin/>}></Route>
+    <Route path='/sign-up' element={<SignUp/>}></Route>
+    <Route path='/about' element={<About/>}></Route>
+    <Route path='/profile' element={<Profile/>}></Route>
+
+
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+
